668d41256185c62d219ca9727664764f59338a35
Trevor Lane <tlane@simplenexus.com>
2019-05-17 15:26:14 -0600

    progress

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,44 @@
 import React from 'react';
 import { createGlobalStyle, ThemeProvider } from "styled-components";
-import { reset, themes, List, ListItem, Divider, Button } from 'react95';
+import { reset, themes, List, ListItem, Divider, Button, Select } from 'react95';
 import '../App.css';
 
 import startSong from '../assets/songs/startSong.mp3';
 import passSong from '../assets/songs/passSong.mp3';
 import failSong from '../assets/songs/failSong.mp3';
 
+import mario from '../assets/images/Mario.png'
+import donkeyKong from '../assets/images/DonkeyKong.png'
 
-import Music from './Music'
 
+import SW from './SW';
 
 const ResetStyles = createGlobalStyle`
   ${reset}
 `;
 
-let testPassed = () => {
-  return (
-  <audio ref={React.createRef()} src={passSong} autoPlay/>
-  )
-}
 
-let testFailed = () => {
-  return (
-    <audio ref={React.createRef()} src={failSong} autoPlay/>
-    )
-}
-let testStarted = () => {
-  console.log('test')
 
-  return (
-    
-    <audio ref={React.createRef()} src={startSong} autoPlay/>
-    )
-}
+const nodes = [
+  {
+    title: "Mario",
+    image: mario,
+    startSong: startSong,
+    passSong: passSong,
+    failSong: failSong,
+    color: "#E60612"
+  },
+  {
+    title: "DonkeyKong",
+    image: donkeyKong,
+    startSong,
+    passSong,
+    failSong,
+    color: "#009688"
+ },
+  
+
+]
 
 
 
@@ -42,11 +47,12 @@ function App() {
     <div className="App">
       <ResetStyles />
       <ThemeProvider theme={themes.default}>
-      <header className="App-header">
-    <Music  name={"Start Test"} url={startSong}/>
-    <Music   name={"Test Passed"}   url={passSong}/>
-    <Music   name={"Test Failed"}  url={failSong}/>
-      </header>
+      <SW nodes={nodes}/>
+      {/* <header className="App-header">
+         <Music  name={"Start Test"} url={startSong}/>
+         <Music   name={"Test Passed"}   url={passSong}/>
+         <Music   name={"Test Failed"}  url={failSong}/>
+      </header> */}
       </ThemeProvider>
     </div>
   );
